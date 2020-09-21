8618fc885ee00d210fd0599da327494d22c861ce
Nick Boyd <nickboyd@gmail.com>
2020-09-21 17:51:25 -0600

    static notebook

--- 0.1.0/versions.js
+++
@@ -1,6 +0,0 @@
-var PUBLISH_ROOT = '/SingleMoleculeLocalization.jl/0.1.0';
-var PUBLISH_VERSION = "0.1.0";
-var PUBLISH_VERSIONS = [
-    ["dev","/SingleMoleculeLocalization.jl/dev/index.html"],
-    ["0.1.0","/SingleMoleculeLocalization.jl/0.1.0/index.html"],
-];
